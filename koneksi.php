1fb7369daedfa73e4091c25f40adc820e8bb8c6a
AgungBarlianSaputra1 <aliviaabadi007@gmail.com>
2026-05-07 15:08:12 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,15 @@
 <?php
-// PHP akan mencoba mengambil data asli dari Railway dulu, 
+
+header("Access-Control-Allow-Origin: https://agung-php-production-7971.up.railway.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+// PHP akan mencoba mengambil data asli dari Railway dulu,
 // kalau tidak ada baru pakai data di dalam kutip.
 $host = getenv('MYSQLHOST') ?: "mysql.railway.internal";
 $user = getenv('MYSQLUSER') ?: "root";
@@ -13,4 +23,6 @@ if (!$koneksi) {
     // Menampilkan error yang lebih spesifik jika gagal
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
+echo "Koneksi berhasil!";
 ?>
